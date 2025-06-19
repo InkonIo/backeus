03e1330d924b562e7641ee795093bd4d8c3702c4
InkonIo <loke90@bk.ru>
2025-06-19 11:29:43 +0500

    envi

--- src/main/java/com/agrofarm/backend/BackendApplication.java
+++ src/main/java/com/agrofarm/backend/BackendApplication.java
@@ -13,10 +13,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class BackendApplication {
 
   public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure()
-    .directory("target") // путь к папке с .env
-    .filename(".env")
-    .load();
+    Dotenv dotenv = Dotenv.configure().load();
 
     String openaiKey = dotenv.get("OPENAI_API_KEY");
     String dbUser = dotenv.get("POSTGRES_USERNAME");
