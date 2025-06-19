37d011f146ba87b9dfaf386143a8eeaee9cb4742
InkonIo <loke90@bk.ru>
2025-06-19 16:17:24 +0500

    fetch

--- src/main/java/com/agrofarm/backend/config/SecurityConfig.java
+++ src/main/java/com/agrofarm/backend/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true); // если используешь куки
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
